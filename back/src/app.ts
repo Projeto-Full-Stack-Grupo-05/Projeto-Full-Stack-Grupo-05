0953fab3731f25a9b7a397248fc14cc97e183318
marigv95 <mama_guima04@hotmail.com>
2023-08-07 16:38:18 -0300

    Feat/BackOrganization - Folder organization

---
+++ back/src/app.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import { handleErros } from "./error";
+import salesRoutes from "./routes/sales.routes";
+// import cors from "cors";
+
+const app = express();
+app.use(express.json());
+// app.use(cors({ origin: "http://localhost:5173" }));
+app.use("/sales", salesRoutes);
+
+app.use(handleErros);
+
+export default app;
