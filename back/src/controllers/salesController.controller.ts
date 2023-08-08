0b727c16c61c9b0cc8c4a38a672c5eae6ba7f40e
Djeff <djeff.walla99@gmail.com>
2023-08-08 16:03:05 -0300

    feat: saleUpdate and saleDelete

--- back/src/controllers/salesController.controller.ts
+++ back/src/controllers/salesController.controller.ts
@@ -1,4 +1,6 @@
 import { Response, Request } from "express";
+import deleteSaleService from "../services/sales/deleteSaleService.service";
+import updateSaleService from "../services/sales/updateSaleService.service";
 
 
 const createSalesController = async (req: Request, res: Response) => {
@@ -6,4 +8,28 @@ const createSalesController = async (req: Request, res: Response) => {
   
     return res.status(201).json('newSales');
   };
-  
+
+export const updateSaleController = async (
+    req: Request,
+    res: Response
+): Promise<Response> => {
+    const saleData: TSaleRequestUpdate = req.body
+    const saleId: string = req.params.id
+ 
+    const newSaleData:TSale = await updateSaleService(
+        saleData,
+        saleId
+    )
+    return res.status(200).json(newSaleData)
+}
+
+export const deleteSaleController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const saleId: string = req.params.id
+
+  await deleteSaleService(saleId)
+
+  return res.status(204).send()
+}
