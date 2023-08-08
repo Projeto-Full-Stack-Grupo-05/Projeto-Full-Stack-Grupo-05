22d7f578accdcf5338280a0c7afc01802c651381
Ranier_Dias <ranierfernandodias@gmail.com>
2023-08-08 14:19:54 -0300

    feat: add configs to apis

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { isAxiosError } from "axios";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- front/src/pages/HomePage/index.tsx
+++ front/src/pages/HomePage/index.tsx
@@ -1,9 +1,44 @@
 import { HeaderLoggedOut } from "../../components/HeaderLoggedOut";
 import { Footer } from "../../components/Footer";
+import { useQuery } from "react-query";
+import kenzieApi from "../../services/kenzie-car";
+import { IKenzieCar } from "../../services/kenzie-car/interfaces";
+import { isAxiosError } from "axios";
+import ErrorPage from "../ErrorPage";
 
-export const Homepage = () => (
-  <div>
-    <HeaderLoggedOut />
-    <Footer />
-  </div>
-);
+export const Homepage = () => {
+  const getKenzieCars = async () => {
+    try {
+      const cars = await kenzieApi.get<IKenzieCar[]>("/cars?brand=chevrolet");
+
+      return cars.data;
+    } catch (error) {
+      if (isAxiosError(error)) {
+        if (error.status === 500) throw error;
+
+        if (error.status === undefined) throw error;
+      }
+    }
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: "cars",
+    queryFn: getKenzieCars,
+  });
+
+  if (error) return <ErrorPage />;
+
+  return (
+    <div>
+      <HeaderLoggedOut />
+      <ul>
+        {isLoading ? (
+          <p>Carregando...</p>
+        ) : (
+          data!.map((car) => <li>{car.name}</li>)
+        )}
+      </ul>
+      <Footer />
+    </div>
+  );
+};
