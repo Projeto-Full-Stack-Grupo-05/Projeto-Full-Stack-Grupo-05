cab95bbf8ac658cc691eb909c248ebb95e492e15
marigv95 <mama_guima04@hotmail.com>
2023-08-09 15:38:31 -0300

    Create Delete Route

--- back/src/routes/sales.routes.ts
+++ back/src/routes/sales.routes.ts
@@ -2,20 +2,31 @@ import { Router } from "express";
 
 import { createSalesController } from "../controllers/salesController.controller";
 import ensureBodyValidMiddleware from "../middleware/ensureBodyValidMiddleware";
-import { deleteSaleController, updateSaleController } from "../controllers/salesController.controller";
+import {
+  deleteSaleController,
+  updateSaleController,
+} from "../controllers/salesController.controller";
 import ensureIdMiddleware from "../middleware/ensureIdMiddleware";
-import { salesSchemaRequest, salesSchemaUpdateRequest } from "../schemas/salesSchema.schema";
+import {
+  salesSchemaRequest,
+  salesSchemaUpdateRequest,
+} from "../schemas/salesSchema.schema";
 import ensureAlreadyExistsSalesMiddleware from "../middleware/ensureAlreadyExistsSalesMiddleware.middleware";
 
 const salesRoutes = Router();
-salesRoutes.post("",ensureBodyValidMiddleware(salesSchemaRequest),ensureAlreadyExistsSalesMiddleware, createSalesController);
+salesRoutes.post(
+  "",
+  ensureBodyValidMiddleware(salesSchemaRequest),
+  ensureAlreadyExistsSalesMiddleware,
+  createSalesController
+);
 
-
-
-
-salesRoutes.patch('/:id', ensureBodyValidMiddleware(salesSchemaUpdateRequest), ensureIdMiddleware, updateSaleController)
-salesRoutes.delete('/:id', ensureIdMiddleware, deleteSaleController)
-
-
+salesRoutes.patch(
+  "/:id",
+  ensureBodyValidMiddleware(salesSchemaUpdateRequest),
+  ensureIdMiddleware,
+  updateSaleController
+);
+salesRoutes.delete("/:id", ensureIdMiddleware, deleteSaleController);
 
 export default salesRoutes;
