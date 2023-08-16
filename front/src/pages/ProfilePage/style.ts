2158b7ba3409fd22882fba1e95a4190aa53b0e69
Ranier_Dias <ranierfernandodias@gmail.com>
2023-08-16 11:42:21 -0300

    feat: add page profile view

--- front/src/pages/ProfilePage/style.ts
+++ front/src/pages/ProfilePage/style.ts
@@ -1,5 +1,73 @@
 import { styled } from "styled-components";
 
 export const Main = styled.main`
-  height: 90vh;
+  min-height: 90vh;
+
+  > div {
+    > section {
+      margin-top: 26px;
+
+      h2 {
+        font-size: 1.5rem;
+        margin-bottom: 40px;
+      }
+
+      ul {
+        display: flex;
+        align-items: center;
+        gap: 12px;
+        padding: 18px 0;
+        overflow: scroll;
+
+        @media (min-width: 720px) {
+          display: grid;
+          grid-template-columns: repeat(2, auto);
+          justify-content: center;
+          gap: 45px;
+        }
+
+        @media (min-width: 1068px) {
+          grid-template-columns: repeat(3, auto);
+        }
+
+        @media (min-width: 1475px) {
+          grid-template-columns: repeat(4, auto);
+        }
+      }
+
+      > nav {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+        margin: 100px 0 50px 0;
+
+        div {
+          span:first-child {
+            color: var(--color-grey-2);
+          }
+
+          span {
+            font-size: 1.5rem;
+            color: var(--color-grey-3);
+          }
+        }
+
+        > a {
+          text-decoration: none;
+          font-size: 1.5rem;
+          color: var(--color-brand-2);
+        }
+      }
+    }
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 0 16px;
 `;
