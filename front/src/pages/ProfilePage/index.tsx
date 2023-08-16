b23c341fa4eb68cfa0af20e58a58ff58dc8e38f6
Matheus <matheustdl02@gmail.com>
2023-08-16 17:33:12 -0300

    finalizada_html-css

--- front/src/pages/ProfilePage/index.tsx
+++ front/src/pages/ProfilePage/index.tsx
@@ -1,12 +1,40 @@
 import { Footer } from "../../components/Footer";
 import { HeaderLoggedIn } from "../../components/HeaderLoggedIn";
-import { Main } from "./style";
+import {
+  ContainerSection,
+  DivSectionBGDGray,
+  Divposition,
+  Main,
+  SectionPerfil,
+} from "./style";
 
 export default function ProfilePage() {
   return (
     <>
       <HeaderLoggedIn />
+      <Divposition />
+      <SectionPerfil>
+        <ContainerSection>
+          <img src="" alt="foto perfil" />
 
+          <div>
+            <h6>Samuel Leão</h6>
+            <span>Anunciante</span>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500sLorem Ipsum is
+            simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s
+          </p>
+          <button>Criar anuncio</button>
+        </ContainerSection>
+      </SectionPerfil>
+      <DivSectionBGDGray />
       <Main>
         <h2>Profile View</h2>
       </Main>
