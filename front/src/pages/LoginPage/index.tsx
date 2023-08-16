b8e06d6ec49d660819c5c656e1ba34c2d8d36ec4
pedrohenriquebarrosn <pedronascimentobr@hotmail.com>
2023-08-16 18:26:33 -0300

    feat: LoginForm

--- front/src/pages/LoginPage/index.tsx
+++ front/src/pages/LoginPage/index.tsx
@@ -1,9 +1,62 @@
 import { HeaderLoggedOut } from "../../components/HeaderLoggedOut";
 import { Footer } from "../../components/Footer";
+import { StyledDiv, StyledLoginForm, StyledLoginPage } from "./styles";
+import { Input } from "../../components/Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useContext } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { LoginSchema } from "./loginSchema";
+import { UserContext } from "../../context/UserContext/UserContext";
+import { ILoginFormValues } from "../../context/UserContext/@types";
+import { Link } from "react-router-dom";
 
-export const LoginPage = () => (
-  <>
-    <HeaderLoggedOut />
-    <Footer />
-  </>
-);
+export const LoginPage = () => {
+  const { userLogin } = useContext(UserContext);
+
+  const {
+    handleSubmit,
+    register,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    formState: { errors },
+    reset,
+  } = useForm<ILoginFormValues>({ resolver: yupResolver(LoginSchema) });
+
+  const submit: SubmitHandler<ILoginFormValues> = (formData) => {
+    userLogin(formData);
+    reset();
+  };
+
+  return (
+    <>
+      <HeaderLoggedOut />
+      <StyledLoginPage>
+        <StyledLoginForm onSubmit={handleSubmit(submit)}>
+          <h2>Login</h2>
+          <Input
+            label={"Email"}
+            type={"email"}
+            placeholder={"Digitar email"}
+            register={register("email")}
+            error={errors.email}
+          />
+          <div className="forgetPasswordDiv">
+            <Input
+              label={"Senha"}
+              type={"password"}
+              placeholder={"Digitar senha"}
+              register={register("password")}
+              error={errors.password}
+            />
+            <button type="button">Esqueci minha senha</button>
+          </div>
+          <StyledDiv>
+            <button type="submit">Entrar</button>
+            <h4>Ainda não possui conta?</h4>
+            <Link to="/register">Cadastrar</Link>
+          </StyledDiv>
+        </StyledLoginForm>
+      </StyledLoginPage>
+      <Footer />
+    </>
+  );
+};
---
+++ front/src/pages/LoginPage/styles.ts
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+
+export const StyledLoginPage = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f3f5;
+  padding: 10px;
+`;
+
+export const StyledLoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  width: 100%;
+  max-width: 412px;
+  padding-top: 30px;
+
+  background: #fff;
+  border-radius: 4px;
+  padding: 0 45px;
+  margin-top: 200px;
+  margin-bottom: 100px;
+  gap: 30px;
+
+  h2 {
+    width: 100%;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 30px;
+    padding-top: 45px;
+  }
+
+  .forgetPasswordDiv {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+
+    button {
+      position: absolute;
+      top: 70px;
+      right: 0;
+
+      border: none;
+      width: 150px;
+      background-color: transparent;
+      margin-top: 10px;
+
+      font-weight: 600;
+      font-size: 14px;
+      line-height: 24px;
+
+      color: #495057;
+    }
+  }
+`;
+
+export const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-top: 25px;
+  width: 100%;
+  padding-bottom: 45px;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    padding: 12px 28px;
+    cursor: pointer;
+    height: 48px;
+
+    background: none;
+    border: 2.3px solid var(--color-grey-4);
+    border-radius: 4px;
+    text-decoration: none;
+
+    font-weight: 600;
+    font-size: 16px;
+    color: var(--color-grey-0);
+
+    &:hover {
+      color: white;
+      background-color: black;
+      transition: 0.7s;
+      border: 2.3px solid black;
+    }
+  }
+
+  h4 {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+  }
+
+  button {
+    border: none;
+    border-radius: 4px;
+    height: 48px;
+    width: 100%;
+    background-color: var(--color-brand-1);
+
+    font-weight: 600;
+    font-size: 16px;
+    color: #fff;
+  }
+`;
