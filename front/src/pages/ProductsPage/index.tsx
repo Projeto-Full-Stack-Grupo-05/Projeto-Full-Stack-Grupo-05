6d3487afbb688f3bd05bf52a44b538e12141e1db
pedrohenriquebarrosn <pedronascimentobr@hotmail.com>
2023-08-30 10:43:02 -0300

    feat: ProductsPage style

--- front/src/pages/ProductsPage/index.tsx
+++ front/src/pages/ProductsPage/index.tsx
@@ -1,52 +1,154 @@
 import { Footer } from "../../components/Footer";
 import { HeaderLoggedIn } from "../../components/HeaderLoggedIn";
+import { StyledMain } from "./style";
+import car from "../../assets/car.svg";
+import miniCar from "../../assets/miniCar.svg";
+import { Link } from "react-router-dom";
 
 export const ProductsPage = () => {
   return (
     <>
       <HeaderLoggedIn />
-      <main>
-        <section>
-          <div>
-            <img src="" alt="" />
-          </div>
-          <div>
-            <h1>mercedes</h1>
-          </div>
-          <div>
-            <h4>Descrição</h4>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-          <div>
-            <h4>Comentários</h4>
-            <div>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and
-                typesetting.
-              </p>
+      <StyledMain>
+        <div className="MainSection">
+          <div className="divOne">
+            <div className="imgDiv">
+              <img src={car} alt="Car" />
+            </div>
+            <div className="detailsDiv">
+              <h1>
+                Mercedes Benz A 200 CGI ADVANCE SEDAN Mercedes Benz A 200{" "}
+              </h1>
+              <div className="priceDiv">
+                <div className="yearDiv">
+                  <div className="cardDiv">
+                    <h6>2013</h6>
+                  </div>
+                  <div className="cardDiv">
+                    <h6>0 KM</h6>
+                  </div>
+                </div>
+                <h5>R$ 00.000,00</h5>
+              </div>
+              <button>Comprar</button>
+            </div>
+            <div className="desDiv">
+              <h1>Descrição</h1>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
-                text.
-              </p>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and
-                typesetting.
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
               </p>
             </div>
           </div>
-          <div></div>
-        </section>
-        <section>
-          <div></div>
-          <div></div>
-        </section>
-      </main>
+          <div className="asideSection">
+            <div className="firstDiv">
+              <h4>Fotos</h4>
+              <div className="imagesDiv">
+                <div className="imageDiv">
+                  <img src={miniCar} alt="Mini car" />
+                </div>
+                <div className="imageDiv">
+                  <img src={miniCar} alt="Mini car" />
+                </div>
+                <div className="imageDiv">
+                  <img src={miniCar} alt="Mini car" />
+                </div>
+                <div className="imageDiv">
+                  <img src={miniCar} alt="Mini car" />
+                </div>
+                <div className="imageDiv">
+                  <img src={miniCar} alt="Mini car" />
+                </div>
+                <div className="imageDiv">
+                  <img src={miniCar} alt="Mini car" />
+                </div>
+              </div>
+            </div>
+            <div className="profileDiv">
+              <div className="ball"></div>
+              <h4>Samuel leão</h4>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's
+              </p>
+              <Link to="/dashboard" className="ads">
+                Ver todos anuncios
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="secondSection">
+          <div className="divTwo">
+            <div className="commentsContainerDiv">
+              <h1>Comentários</h1>
+              <div className="commentsDiv">
+                <div className="commentDiv">
+                  <div className="profileDiv">
+                    <div className="ball"></div>
+                    <h5>Júlia lima</h5>
+                    <div className="miniBall"></div>
+                    <h6>Há 6 dias</h6>
+                  </div>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                </div>
+                <div className="commentDiv">
+                  <div className="profileDiv">
+                    <div className="ball"></div>
+                    <h5>Júlia lima</h5>
+                    <div className="miniBall"></div>
+                    <h6>Há 6 dias</h6>
+                  </div>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                </div>
+                <div className="commentDiv">
+                  <div className="profileDiv">
+                    <div className="ball"></div>
+                    <h5>Júlia lima</h5>
+                    <div className="miniBall"></div>
+                    <h6>Há 6 dias</h6>
+                  </div>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="postDiv">
+              <div className="profileDiv">
+                <div className="ball"></div>
+                <h5>Samuel leão</h5>
+              </div>
+              <input type="text" />
+              <div className="optionsDiv">
+                <div className="cards">Gostei muito!</div>
+                <div className="cards">Incrível</div>
+                <div className="cards">Recomendarei para meus amigos!</div>
+              </div>
+            </div>
+          </div>
+          <div className="asidePositionDiv">
+            <div className="positionDiv"></div>
+          </div>
+        </div>
+      </StyledMain>
       <Footer />
     </>
   );
---
+++ front/src/pages/ProductsPage/style.ts
@@ -0,0 +1,468 @@
+import { styled } from "styled-components";
+
+export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 5px;
+  background-color: #f1f3f5;
+  padding: 10px;
+  align-items: center;
+  justify-content: center;
+
+  .MainSection {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: flex-start;
+    margin-top: 25px;
+    gap: 30px;
+
+    .divOne {
+      .imgDiv {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        max-width: 750px;
+        background-color: #fff;
+        padding-top: 60px;
+        padding-bottom: 35px;
+
+        border-radius: 4px;
+
+        @media (max-width: 870px) {
+          max-width: 1000px;
+        }
+
+        @media (max-width: 430px) {
+          img {
+            width: 100%;
+          }
+        }
+      }
+
+      .detailsDiv {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        max-width: 750px;
+        gap: 20px;
+
+        border-radius: 4px;
+
+        background-color: #fff;
+        padding: 30px;
+
+        @media (max-width: 870px) {
+          max-width: 1000px;
+        }
+
+        h1 {
+          font-weight: 600;
+          font-size: 20px;
+          line-height: 25px;
+          margin-top: 25px;
+        }
+
+        .priceDiv {
+          display: flex;
+          justify-content: space-between;
+          margin-top: 25px;
+          width: 100%;
+          max-width: 657px;
+
+          h5 {
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 20px;
+          }
+
+          .yearDiv {
+            display: flex;
+            gap: 10px;
+
+            .cardDiv {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+
+              border-radius: 4px;
+
+              width: 52px;
+              height: 32px;
+              background-color: var(--color-brand-4);
+
+              h6 {
+                font-weight: 500;
+                font-size: 14px;
+                line-height: 24px;
+                color: var(--color-brand-1);
+              }
+            }
+          }
+        }
+      }
+
+      button {
+        width: 100px;
+        min-height: 38px;
+        border-radius: 4px;
+        border: none;
+        background-color: var(--color-brand-1);
+        color: #fff;
+      }
+    }
+
+    .asideSection {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      @media (max-width: 870px) {
+        width: 100%;
+      }
+
+      .firstDiv {
+        width: 100%;
+        max-width: 440px;
+        background-color: #fff;
+
+        border-radius: 4px;
+
+        padding: 30px;
+
+        @media (max-width: 870px) {
+          max-width: 1000px;
+        }
+
+        h4 {
+          width: 100%;
+          font-weight: 600;
+          font-size: 20px;
+          line-height: 25px;
+          margin-bottom: 30px;
+
+          color: rgba(33, 37, 41, 1);
+        }
+      }
+
+      .imagesDiv {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 30px;
+
+        @media (max-width: 530px) {
+          grid-template-columns: repeat(2, 1fr);
+        }
+
+        .imageDiv {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 4px;
+
+          height: 108px;
+          width: 100%;
+          background-color: rgba(233, 236, 239, 1);
+        }
+      }
+
+      .profileDiv {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 27px;
+        gap: 15px;
+        width: 100%;
+        max-width: 440px;
+        background-color: #fff;
+        border-radius: 4px;
+        padding: 30px;
+
+        @media (max-width: 870px) {
+          max-width: 1000px;
+        }
+
+        .ball {
+          height: 104px;
+          width: 104px;
+          border-radius: 100%;
+          margin-top: 10px;
+
+          background-color: var(--color-brand-1);
+        }
+
+        h4 {
+          font-weight: 600;
+          font-size: 20px;
+          line-height: 25px;
+
+          color: rgba(33, 37, 41, 1);
+        }
+
+        p {
+          font-weight: 400;
+          font-size: 16px;
+          line-height: 28px;
+          margin-top: 20px;
+          color: rgba(73, 80, 87, 1);
+        }
+
+        .ads {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-top: 20px;
+
+          width: 100%;
+          max-width: 206px;
+          padding: 12px 28px;
+          cursor: pointer;
+          height: 48px;
+
+          background: var(--color-grey-0);
+
+          border-radius: 4px;
+          text-decoration: none;
+
+          font-weight: 600;
+          font-size: 14px;
+          color: var(--color-white-fixed);
+
+          &:hover {
+            color: var(--color-grey-0);
+            color: var(--color-white-fixed);
+            transition: 0.7s;
+            border: 2.3px solid black;
+          }
+        }
+      }
+    }
+
+    .desDiv {
+      display: flex;
+      flex-direction: column;
+      margin-top: 30px;
+
+      width: 100%;
+      max-width: 750px;
+      background-color: #fff;
+
+      border-radius: 4px;
+
+      padding: 30px;
+
+      @media (max-width: 870px) {
+        max-width: 1000px;
+      }
+
+      h1 {
+        font-weight: 600;
+        font-size: 20px;
+        line-height: 25px;
+        margin-top: 15px;
+      }
+
+      p {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 28px;
+        margin-top: 33px;
+        margin-bottom: 20px;
+        color: rgba(73, 80, 87, 1);
+      }
+    }
+
+    @media (max-width: 870px) {
+      flex-direction: column;
+    }
+  }
+
+  .secondSection {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    gap: 30px;
+
+    @media (max-width: 870px) {
+      justify-content: flex-start;
+    }
+
+    .divTwo {
+      .commentsContainerDiv {
+        display: flex;
+        flex-direction: column;
+        margin-top: 30px;
+        width: 100%;
+        max-width: 750px;
+        background-color: #fff;
+        border-radius: 4px;
+        padding: 30px;
+
+        @media (max-width: 870px) {
+          max-width: 1000px;
+        }
+
+        h1 {
+          font-weight: 600;
+          font-size: 20px;
+          line-height: 25px;
+          margin-top: 15px;
+        }
+
+        .commentsDiv {
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          gap: 30px;
+          margin-top: 15px;
+
+          .commentDiv {
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            gap: 15px;
+            margin-top: 30px;
+
+            .profileDiv {
+              display: flex;
+              align-items: center;
+              gap: 10px;
+
+              .ball {
+                width: 20px;
+                height: 20px;
+                border-radius: 100%;
+                background-color: black;
+              }
+
+              h5 {
+                font-weight: 500;
+                font-size: 14px;
+                line-height: 24px;
+                color: rgba(33, 37, 41, 1);
+              }
+
+              .miniBall {
+                width: 4px;
+                height: 4px;
+                border-radius: 100%;
+                background-color: rgba(134, 142, 150, 1);
+                margin-top: 2px;
+              }
+
+              h6 {
+                font-weight: 400;
+                font-size: 12px;
+                line-height: 24px;
+                color: rgba(134, 142, 150, 1);
+                margin-top: 3px;
+                padding-left: 3px;
+              }
+            }
+
+            p {
+              font-weight: 400;
+              font-size: 14px;
+              line-height: 24px;
+              color: rgba(73, 80, 87, 1);
+            }
+          }
+        }
+      }
+
+      .postDiv {
+        display: flex;
+        flex-direction: column;
+        margin-top: 15px;
+        gap: 15px;
+        margin-bottom: 60px;
+
+        width: 100%;
+        max-width: 750px;
+        background-color: #fff;
+
+        border-radius: 4px;
+
+        padding: 30px;
+
+        @media (max-width: 870px) {
+          max-width: 1000px;
+        }
+
+        .profileDiv {
+          display: flex;
+          align-items: center;
+          gap: 10px;
+          width: 100%;
+
+          .ball {
+            width: 20px;
+            height: 20px;
+            border-radius: 100%;
+            background-color: black;
+          }
+
+          h5 {
+            font-weight: 500;
+            font-size: 14px;
+            line-height: 24px;
+            color: rgba(33, 37, 41, 1);
+          }
+        }
+
+        .optionsDiv {
+          display: flex;
+          gap: 5px;
+
+          @media (max-width: 870px) {
+            flex-direction: column;
+          }
+
+          input {
+          }
+
+          .cards {
+            display: flex;
+            align-items: center;
+            height: 24px;
+            padding-left: 12px;
+            padding-right: 12px;
+
+            border-radius: 24px;
+            color: rgba(134, 142, 150, 1);
+            background-color: rgba(233, 236, 239, 1);
+
+            font-weight: 500;
+            font-size: 12px;
+            line-height: 24px;
+          }
+        }
+      }
+    }
+
+    .asidePositionDiv {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      width: 100%;
+      max-width: 440px;
+
+      @media (max-width: 870px) {
+        display: none;
+      }
+
+      .positionDiv {
+        width: 100%;
+        max-width: 440px;
+        background-color: transparent;
+        border-radius: 4px;
+        padding: 30px;
+
+        @media (max-width: 870px) {
+          display: none;
+        }
+      }
+    }
+  }
+`;
